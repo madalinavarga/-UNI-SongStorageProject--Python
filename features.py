9265979f12c428ffd97c8733e9d5a163f5d1b7cf
varga madalina <madalina.g.varga@gmail.com>
2022-11-26 16:22:14 +0200

    read input and check command

---
+++ features.py
@@ -0,0 +1,24 @@
+
+def add_song(params):
+    print(params)
+    return
+
+
+def delete_song(params):
+    print(params)
+    return
+
+
+def modify_data(params):
+    print(params)
+    return
+
+
+def search(params):
+    print(params)
+    return
+
+
+def create_save_list(params):
+    print(params)
+    return
